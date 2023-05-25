9cb5160446ae140b2d86af3c0028a4e59c9b212b
Michael Lorton <malvoliosf@gmail.com>
2023-05-24 17:22:50 -0700

    improved API

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,26 @@ import search from "./search";
 
 const optionDefinitions = [
   { name: "index", alias: "i", type: String },
+  { name: "contains", alias: "c", type: Boolean },
   { name: "src", type: String, multiple: true, defaultOption: true },
 ];
 
 const options = commandLineArgs(optionDefinitions);
 
-console.log(search(options.index)(options.src.join(" ")));
+const searchString = options.src.join(" ");
+
+console.log(`Searching for '${searchString}'`);
+
+const results = search({
+  indexDir: options.index,
+  containSearch: options.contains,
+})(searchString);
+
+if (results.length) {
+  console.log(`${results.length} file(s) found.`);
+  results.forEach((file) => {
+    console.log(`* ${file}`);
+  });
+} else {
+  console.log("Sorry, no matching files found");
+}
--- src/search.ts
+++ src/search.ts
@@ -1,23 +1,34 @@
 import fs from "fs";
 import { extractTokens } from "./lib/tokens";
 import { getArrayUnion, getArrayIntersection } from "./lib/setops";
-const search = (indexDir: string) => {
+
+type SearchOptions = {
+  indexDir: string;
+  containSearch?: boolean;
+};
+
+const search = ({ indexDir, containSearch }: SearchOptions) => {
   const findAllDerivedTokens = (baseToken: string) => {
-    const files = fs.readdirSync(indexDir);
-    return files.filter((file) => file.startsWith(baseToken));
+    const allTokens = fs.readdirSync(indexDir);
+    return allTokens.filter((file) =>
+      containSearch ? file.includes(baseToken) : file.startsWith(baseToken)
+    );
   };
 
   const loadMatchIndex = (token: string): string[] =>
-    fs.readFileSync(`${indexDir}/${token}`, "utf8").split("\n");
+    fs
+      .readFileSync(`${indexDir}/${token}`, "utf8")
+      .split("\n")
+      .filter((match) => !!match);
 
-  const findAllMatches = (baseToken: string): string[] => {
+  const findAllMatchesForToken = (baseToken: string): string[] => {
     const tokens = findAllDerivedTokens(baseToken);
     return getArrayUnion(tokens.map(loadMatchIndex));
   };
 
   return (searchString: string) => {
     const tokens = extractTokens(searchString);
-    const files = Array.from(tokens).map(findAllMatches);
+    const files = Array.from(tokens).map(findAllMatchesForToken);
     return getArrayIntersection(files);
   };
 };
